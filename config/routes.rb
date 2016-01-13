f2b9369ecc777c49abe9989d53ffc21bf0ca2bf4
keithrayan <keithrayan@comcast.net>
2016-01-13 12:41:42 +0000

    signup users from ui

--- config/routes.rb
+++ config/routes.rb
@@ -8,6 +8,9 @@ Rails.application.routes.draw do
   get 'about', to: 'pages#about'
   resources :articles
   
+  get 'signup', to: 'users#new'
+  resources :users, except: [:new]
+  
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
  
